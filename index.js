5a88e7a16804fb04522b476c274deb75881a7f19
unknown <prathamjagga123@gmail.com>
2023-10-28 02:43:49 +0530

    specify port

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ app.get("/", (req, res) => {
 	res.json({ success: "true" });
 });
 
-app.listen(5000, () => {
+app.listen(process.env.PORT || 5000, () => {
 	console.log("Server started on port 5000");
 });
